030b2d440d5d09b99500cb1db06d44202d84779f
mikeyfoo <realfcbstuff@yahoo.co.uk>
2015-12-23 00:51:32 +0800

    1)seperate pmv and ppd for comfpmv and comfPMVElevatedAirspeed with 2 additional wrapper

--- contrib/comfort_test.py
+++ contrib/comfort_test.py
@@ -3,20 +3,34 @@ __author__ = 'Mikael Foo'
 import ComfortModelsClass
 
 dumpmv = ComfortModelsClass.ComfortModels()
-def comfpmv(self, ta, tr, vel, rh, met, clo, wme):
  #%relative air velocity (m/s)
  #%relative humidity (%) Used only this way to input humidity level
  #%metabolic rate (met)
  #%clothing (clo),0.5 typical indoor summer clothing
  #%external work, normally around 0 (met)
 
-    return dumpmv.comfPMV(ta, tr, vel, rh, met, clo, wme)
+def comfpmv(self, ta, tr, vel, rh, met, clo, wme):
 
 
-def comfPMVElevatedAirspeed(self, ta, tr, vel, rh, met, clo, wme):
+    comf = dumpmv.comfPMV(ta, tr, vel, rh, met, clo, wme)
+
+    return comf[0]
+
+def comfpmv_ppd(self, ta, tr, vel, rh, met, clo, wme):
+
+    comf = dumpmv.comfPMV(ta, tr, vel, rh, met, clo, wme)
+    return comf[1]
 
 
-    return dumpmv.comfPMVElevatedAirspeed(ta, tr, vel, rh, met, clo, wme)
+def comfPMVElevatedAirspeed_pmv(self, ta, tr, vel, rh, met, clo, wme):
+
+    comf = dumpmv.comfPMVElevatedAirspeed(ta, tr, vel, rh, met, clo, wme)
+    return comf[0]
+
+def comfPMVElevatedAirspeed_ppd(self, ta, tr, vel, rh, met, clo, wme):
+
+    comf = dumpmv.comfPMVElevatedAirspeed(ta, tr, vel, rh, met, clo, wme)
+    return comf[1]
 
 
 
